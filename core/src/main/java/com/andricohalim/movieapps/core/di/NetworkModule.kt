def0b43f08b7d6b8956ca30f3588d7f3ca25e384
Andrico <103520236+AndricoHalim@users.noreply.github.com>
2024-02-12 01:43:27 +0700

    implement certificate pinning

--- core/src/main/java/com/andricohalim/movieapps/core/di/NetworkModule.kt
+++ core/src/main/java/com/andricohalim/movieapps/core/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,6 +16,12 @@ import java.util.concurrent.TimeUnit
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
+    private val hostname = "api.themoviedb.org"
+    private val certificatePinner = CertificatePinner.Builder()
+        .add(hostname, "sha256/5VLcahb6x4EvvFrCF2TePZulWqrLHS2jCg9Ywv6JHog=")
+        .add(hostname, "sha256/vxRon/El5KuI4vx5ey1DgmsYmRY0nDd5Cg4GfJ8S+bg=")
+        .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+        .build()
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
@@ -29,6 +36,7 @@ class NetworkModule {
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
