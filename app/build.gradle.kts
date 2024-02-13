db1d8ae1dafd920e97359b81748d9530862a8f51
Andrico <103520236+AndricoHalim@users.noreply.github.com>
2024-02-13 14:19:29 +0700

    fix configg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,9 +55,4 @@ android {
 
 dependencies {
     implementation(project(":core"))
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.squareup.leakcanary:leakcanary-android:2.13")
-    implementation("com.airbnb.android:lottie:6.3.0")
 }
