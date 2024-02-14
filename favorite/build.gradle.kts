e1280db78953a6a437f7dfe60f46b5faf8df9280
Andrico <103520236+AndricoHalim@users.noreply.github.com>
2024-02-15 01:54:23 +0700

    change gradle

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -32,5 +32,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.annotation:annotation:1.1.0")
+    androidTestImplementation("androidx.annotation:annotation:1.1.0")
     implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
 }
